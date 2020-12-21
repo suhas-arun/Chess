370715df6bc4cc3feaafe74c607ed409b4b001a6
Suhas Arun <suhas.arun142@gmail.com>
2020-12-21 23:59:25 +0000

    Version 5: Added Rook class

--- main.py
+++ main.py
@@ -152,3 +152,30 @@ class Knight(Piece):
                 moves.append((new_row, new_column))
 
         return moves
+
+
+class Rook(Piece):
+    """Piece with value 5"""
+    
+    def __init__(self, row, column, colour):
+        super().__init__(row, column, 5, colour)
+        self.has_moved = False
+    
+    def generate_moves(self):
+        """Rooks move up and down their column and row"""
+        moves = []
+
+        # Squares on the same row
+        columns = [i for i in range(8)]
+        columns.pop(self.column)
+        moves += [(self.row, i) for i in columns]
+
+        #Squares on the same column
+        rows = [i for i in range(8)]
+        rows.pop(self.row)
+        moves += [(i, self.column) for i in rows]
+
+        return moves
+
+rook = Rook(2, 1, True)
+print(rook.generate_moves())
